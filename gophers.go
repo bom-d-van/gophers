ba3ee34ff16c99ffa547828fb534f72ef5d9c668
bom_d_van <bom.d.van@gmail.com>
2015-10-24 10:47:56 +0800

    Update docs

--- gophers.go
+++ gophers.go
@@ -1,4 +1,9 @@
 // Gophers runs scripts or commands concurrently and kills them when gophers was killed.
+//
+// Usage
+//  	gophers gulp "find . -iname '*.go' | entr -rd grm"
+//
+// What is grm: https://github.com/bom-d-van/bin/blob/master/grm
 package main
 
 import (
